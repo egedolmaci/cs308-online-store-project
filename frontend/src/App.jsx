2043a3cae684f2656d23759fb7c5062885ad5a35
egeyardimci <ege.yardimci@sabanciuniv.edu>
2025-10-24 21:22:26 +0300

    feat(frontend): initalized redux store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Store from './pages/store'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </Router>
+    </Provider>
   )
 }
 
